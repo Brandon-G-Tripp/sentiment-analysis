c1a44469f5cc12d684dacce15c17a1711e41282e
Brandon Tripp <brandongtripp@gmail.com>
2024-02-14 08:26:51 -0700

    removal of unused imports for initial test

--- eda/tests/test_eda.py
+++ eda/tests/test_eda.py
@@ -1,14 +1,15 @@
+# from eda.nlp_sentiment_eda import calculate_review_lengths
 import unittest
 
 import sys
-sys.path.append('../')
 
-from eda.nlp_sentiment_eda import calculate_review_lengths
+sys.path.append("../")
+
 
 class TestEDA(unittest.TestCase):
     def test_import(self):
-        import nltk
-        import nltk.corpus
+        # import nltk
+        # import nltk.corpus
         self.assertTrue(True)
 
     # def test_review_lengths(self):
@@ -17,5 +18,5 @@ class TestEDA(unittest.TestCase):
     #     self.assertEqual(len(lengths), 100)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
